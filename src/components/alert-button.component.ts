d9d271d5301817b03cad44b1aef7e25dc4c8dd22
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2016-03-18 19:00:55 +0900

    step2実装完了

---
+++ src/components/alert-button.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-alert-button',
+  templateUrl: '/src/components/alert-button.component.html'
+})
+export class AlertButtonComponent {
+}
--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {AlertButtonComponent} from './alert-button.component';
 
 @Component({
   selector: 'my-app',
-  templateUrl: '/src/components/app.component.html'
+  templateUrl: '/src/components/app.component.html',
+  directives: [AlertButtonComponent]
 })
 export class AppComponent {
 }
