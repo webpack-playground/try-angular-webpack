6dc16ad4282a1eb2024a6e7227f7080e592a53ee
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2016-03-18 19:10:51 +0900

    step4実装完了

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,10 +1,17 @@
 import {Component} from 'angular2/core';
 import {AlertButtonComponent} from './alert-button.component';
+import {MyService} from '../services/my.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: '/src/components/app.component.html',
-  directives: [AlertButtonComponent]
+  directives: [AlertButtonComponent],
+  providers: [MyService]
 })
 export class AppComponent {
+  private data: string[];
+
+  constructor(private myService: MyService) {
+    this.data = myService.loadData();
+  }
 }
